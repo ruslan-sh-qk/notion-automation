92ef6dc21a62b3e1bbf9f1619a717c3f61243797
ruslan-sh-qk <ruslanqku@gmail.com>
2025-06-18 10:00:51 +0300

    'feature(TT-2)'

--- scripts/update-notion-approved-by.js
+++ scripts/update-notion-approved-by.js
@@ -2,36 +2,37 @@ const NotionApi = require("./notion.api");
 const GitHubApi = require("./github.api");
 const utils = require("./utils");
 
-async function main(env, { runFunc, NotionApiService }) {
+async function main(env, { run, NotionApi, GitHubApi }) {
     const { getEnvOrThrow } = utils;
+
     const credentials = {
-        author: getEnvOrThrow(env, 'MR_AUTHOR'),
+        mergeRequestLogin: getEnvOrThrow(env, 'MR_AUTHOR'),
         databaseId: getEnvOrThrow(env, 'NOTION_DATABASE_ID'),
         mergeRequestTitle: getEnvOrThrow(env, 'MR_TITLE')
-    }
-
+    };
     const notionToken = getEnvOrThrow(env, 'NOTION_SECRET');
-    const notionApiService = new NotionApiService(notionToken);
 
-    await runFunc({ notionApiService, credentials });
+    const gitHubApi = new GitHubApi();
+    const notionApi = new NotionApi(notionToken);
+
+    await run({ notionApi, credentials, gitHubApi });
 }
 
-async function run({ notionApiService, credentials }) {
-    const { author, databaseId, mergeRequestTitle } = credentials;
-    await notionApiService.healthCheck();
+async function run({ notionApi, credentials, gitHubApi }) {
+    const { mergeRequestLogin, databaseId, mergeRequestTitle } = credentials;
+    await notionApi.healthCheck();
 
-    // const gitHubApi = new GitHubApi();
-    // const { name } = await gitHubApi.getUser(au);
+    const { name: githubUserName } = await gitHubApi.getUser(mergeRequestLogin);
 
     const taskId = utils.parseTicketId(mergeRequestTitle);
 
-    const pageId = await notionApiService.findPageByTaskFromDatabase(taskId, databaseId);
-    await notionApiService.updateApprovedBy(pageId, author);
-    console.log(`Updated Notion page ${ pageId } with author "${ author }" for task "${ taskId }".`); // cover with tests
+    const pageId = await notionApi.findPageByTaskFromDatabase(taskId, databaseId);
+    await notionApi.updateApprovedBy(pageId, githubUserName);
+    console.log(`Updated Notion page ${ pageId } with author "${ githubUserName }" for task "${ taskId }".`); // cover with tests
 }
 
 if ( require.main === module ) {
-    main(process.env, { runFunc: run, NotionApiService: NotionApi }).catch((err) => {
+    main(process.env, { run, NotionApi, GitHubApi }).catch((err) => {
         console.error('Script failed:', err.message);
     });
 }
